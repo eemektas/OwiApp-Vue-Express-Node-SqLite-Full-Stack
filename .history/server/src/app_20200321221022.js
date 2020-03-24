7e50473ba635e7f58f43a9ab6cf99550f9ef10b6
eemektas <eemektas@gmail.com>
2020-03-24 20:13:17 +0300

    app main file

---
+++ .history/server/src/app_20200321221022.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+const fetch = require('node-fetch')
+const { sequelize } = require('./models')
+const config = require('./config/config')
+
+const app = express()
+const router = express.Router()
+
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+// app.use(express.json())
+
+require('./routes')(app)
+
+sequelize.sync({force: true}) // {force: true} in sync func to del all tables
+  .then(() => {
+    app.listen(config.port)
+    console.log(`Server is listening on http://localhost:${config.port}/`)
+  })
---
+++ .history/server/src/app_20200324134544.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+const fetch = require('node-fetch')-Key
+const { sequelize } = require('./models')
+const config = require('./config/config')
+const session = require('express-session')
+const RedisStore = require('connect-redis')(session)
+
+const app = express()
+const router = express.Router()
+
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+app.use(session({
+  store: new RedisStore(redisOpts),
+  secret: 'Secret-Key',
+  resave: false,
+  saveUninitialized: true
+}))
+// app.use(express.json())
+
+require('./routes')(app)
+
+sequelize.sync({force: true}) // {force: true} in sync func to del all tables
+  .then(() => {
+    app.listen(config.port)
+    console.log(`Server is listening on http://localhost:${config.port}/`)
+  })
