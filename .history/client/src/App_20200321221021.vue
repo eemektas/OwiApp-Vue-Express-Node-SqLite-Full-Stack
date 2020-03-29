a951efbf72c0e70e24e321ce69b37cad6b25003b
eemektas <eemektas@gmail.com>
2020-03-29 21:28:16 +0300

    App.vue changes

---
+++ .history/client/src/App_20200321221021.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app" >
+    <v-app class="bg-no-repeat cover bg-white hero relative"
+    :style="{ backgroundImage: `url(${backgroundUrl})` }">
+
+        <page-header />
+
+        <main>
+          <v-container>
+            <router-view></router-view>
+          </v-container>
+        </main>
+    </v-app>
+  </div>
+</template>
+
+<script>
+import PageHeader from '@/components/Header.vue'
+import backgroundUrl from '@/assets/sky.jpg'
+export default {
+  name: 'app',
+  components: {
+    PageHeader
+  },
+  data () {
+    return {
+      backgroundUrl
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+.danger-alert {
+  color: red;
+}
+
+.error {
+  color: red;
+}
+
+h1 {
+  color: white;
+}
+</style>
