4d2539d6a9608e4b6a305e5ab0e1b92f8c863cf1
eemektas <eemektas@gmail.com>
2020-03-29 21:35:42 +0300

    routes

---
+++ .history/server/src/routes_20200328211554.js
@@ -0,0 +1,32 @@
+const AuthController = require('./controllers/AuthController')
+const AuthControllerPolicy = require('./policies/AuthControllerPolicy')
+const LocationController = require('./controllers/LocationController')
+const WeatherController = require('./controllers/WeatherController')
+
+
+module.exports = (app) => {
+  app.get('/register', 
+  AuthController.getInfo)
+
+  app.post('/register', 
+    AuthControllerPolicy.register,
+    AuthController.register)
+
+  app.post('/login',
+    AuthController.login)
+
+  app.get('/locations',
+    LocationController.index)
+
+  app.post('/locations',
+    LocationController.post)
+
+  app.post('/weather',
+    WeatherController.post) 
+
+  app.get('/weather',
+    WeatherController.index)
+
+  app.get('/users',
+    AuthController.index)  
+}
