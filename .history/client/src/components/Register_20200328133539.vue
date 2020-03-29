77feef5d083f1f0a7050800512fece762aa4da69
eemektas <eemektas@gmail.com>
2020-03-29 21:33:28 +0300

    register page

---
+++ .history/client/src/components/Register_20200328133539.vue
@@ -0,0 +1,74 @@
+<template>
+  <v-container
+    mt-10
+    class="fill-height"
+    fluid>
+    <v-layout column mt-10>
+      <br>
+      <h1>Register</h1>
+      <div class="text-xs-center">
+        <v-flex xs6 offset-xs3>
+          <v-expansion-panel>
+            <v-form
+              ref="form"
+              name="owi-form">
+              <v-text-field class="mt-10"
+                dark
+                label="Username"
+                v-model="username">
+                autocomplete="off"
+              </v-text-field>
+              <v-text-field
+                dark
+                label="Password"
+                type="password"
+                v-model="password"
+                autocomplete="new-password">
+              </v-text-field>
+            </v-form>
+            <div class="danger-alert" v-html="error" />
+            <v-btn
+              dark
+              x-large
+              class="dark mt-10"
+              @keypress.enter="register"
+              @click="register">
+              Register
+            </v-btn>
+          </v-expansion-panel>
+        </v-flex>
+      </div>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+import AuthService from '@/services/AuthService'
+export default {
+  data () {
+    return {
+      username: '',
+      password: '',
+      error: null
+    }
+  },
+  methods: {
+    async register () {
+      try {
+        const response = await AuthService.register({
+          username: this.username,
+          password: this.password
+        })
+        this.$store.dispatch('setToken', response.data.token)
+        this.$store.dispatch('setUser', response.data.user)
+        this.$router.push('Login')
+      } catch (error) {
+        this.error = error.response.data.error
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
