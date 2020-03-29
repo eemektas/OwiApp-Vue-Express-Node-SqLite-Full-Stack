a8f542705517b71d2488659392aa3316c063dbd3
eemektas <eemektas@gmail.com>
2020-03-29 21:33:12 +0300

    login page

---
+++ .history/client/src/components/Login_20200326013150.vue
@@ -0,0 +1,83 @@
+<template>
+  <v-container
+    mt-10
+    class="fill-height"
+    fluid>
+    <v-layout column mt-10>
+      <br>
+      <h1>Login</h1>
+      <div class="text-xs-center">
+        <v-flex xs6 offset-xs3>
+            <v-form
+              ref="form"
+              name="owi-form"
+              autocomplete="off">
+              <v-text-field
+                dark
+                class="mt-10"
+                label="Username"
+                v-model="username">
+              </v-text-field>
+              <v-text-field
+                dark
+                label="Password"
+                type="password"
+                v-model="password"
+                autocomplete="new-password">
+              </v-text-field>
+            </v-form>
+            <div class="danger-alert" v-html="error" />
+            <v-btn
+              dark
+              x-large
+              class="dark mt-10"
+              @click="login"
+              >
+              Login
+            </v-btn>
+        </v-flex>
+      </div>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+import AuthService from '@/services/AuthService'
+export default {
+  data () {
+    return {
+      username: '',
+      password: '',
+      error: null
+    }
+  },
+  methods: {
+    async login () {
+      try {
+        const response = await AuthService.login({
+          username: this.username,
+          password: this.password
+        })
+        // const token = response.data.token
+        this.$store.dispatch('setToken', response.data.token)
+        this.$store.dispatch('setUser', response.data.user)
+        if (this.$store.state.isUserLoggedIn) {
+          this.$router.push('Weather')
+        }
+      } catch (error) {
+        this.error = error.response.data.error
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.error {
+  color: red;
+}
+
+h1 {
+  color: white;
+}
+</style>
