94056199b16d5828d43b262bdf7df396334c1c80
eemektas <eemektas@gmail.com>
2020-03-31 17:01:32 +0300

    logout path fixed

---
+++ .history/client/src/components/Header_20200331164343.vue
@@ -0,0 +1,105 @@
+<template>
+  <v-card flat class="mb-10">
+    <v-toolbar dark
+    src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg">
+      <v-toolbar-title class="home disabled display-1 mr-5"
+        tag="span"
+        :to="{
+          name: 'Weather'
+        }">
+        OWI
+      </v-toolbar-title>
+
+      <v-btn
+        v-if="$store.state.isUserLoggedIn"
+        text
+        dark
+        :to="{
+          name: 'Weather'
+        }">
+        Weather
+      </v-btn>
+
+      <v-btn
+        v-if="$store.state.isUserLoggedIn && $store.state.user.username === 'root'"
+        text
+        dark
+        :to="{
+          name: 'Locations'
+        }">
+        Locations
+      </v-btn>
+
+      <v-btn
+        v-if="$store.state.isUserLoggedIn && $store.state.user.username === 'root'"
+        text
+        dark
+        :to="{
+          name: 'Users'
+        }">
+        Users
+      </v-btn>
+
+      <v-btn
+        v-if="$store.state.isUserLoggedIn"
+        text
+        dark
+        :to="{
+          name: 'Reports'
+        }">
+        Reports
+      </v-btn>
+
+    <v-spacer></v-spacer>
+
+      <v-btn
+        v-if="!$store.state.isUserLoggedIn"
+        text
+        dark
+        :to="{
+          name: 'Login'
+        }">
+        Login
+      </v-btn>
+
+      <v-btn
+        v-if="!$store.state.isUserLoggedIn"
+        text
+        dark
+        :to="{
+          name: 'Register'
+        }">
+        Register
+      </v-btn>
+
+      <v-btn
+        v-if="$store.state.isUserLoggedIn"
+        text
+        dark
+        @click="logout"
+        :to="{
+          name: 'Login'
+        }">
+        Log Out
+      </v-btn>
+    </v-toolbar>
+  </v-card>
+</template>
+
+<script>
+export default {
+  methods: {
+    logout () {
+      this.$store.dispatch('setToken', null)
+      this.$store.dispatch('setUser', null)
+      this.$router.push({
+        name: 'Login'
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
