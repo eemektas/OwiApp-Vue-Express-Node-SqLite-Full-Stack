b84076e6dbfc8f355c9346be8363483ea5908dfd
eemektas <eemektas@gmail.com>
2020-03-31 17:02:27 +0300

    auth

---
+++ .history/client/src/services/AuthService_20200331163135.js
@@ -0,0 +1,24 @@
+import Api from '@/services/Api'
+
+export default {
+  index () {
+    return Api().get('users')
+  },
+  register (credentials) {
+    return Api().post('register', credentials)
+  },
+  login (credentials) {
+    return Api().post('login', credentials)
+  },
+  put (user) {
+    return Api().delete(`users/${user.id}`, user)
+  },
+  delete (userId) {
+    return Api().delete(`users/${userId}`)
+  }
+}
+
+// Auth.register({
+//   username: 'root',
+//   password: 'root'
+// })
